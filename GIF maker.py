6246ef622c5a9cda22cb894736fa9ba075528926
iamfahad459 <iamfahad459@gmail.com>
2024-07-16 09:19:19 +0500

    Create GIF maker.py

---
+++ GIF maker.py
@@ -0,0 +1,15 @@
+#Goal is to make a GIF
+
+import sys
+from PIL import Image
+
+images= []
+
+for arg in sys.argv[1:]:
+    image=image.open(arg)
+    images.append(image)
+    
+images[0].save(
+    "costumes.gif",save_all=True,append_images=[images[1]], duration=200 , loop=0
+)    
+    
